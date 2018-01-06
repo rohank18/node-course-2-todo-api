1e60b7a2e8cff53e3582318058c5d57d394d5f00
rohan <rohankhunteta3000@gmail.com>
2018-01-06 23:38:17 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ app.get('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
 
-    res.send({todo});
+    res.send({todo}); 
   }).catch((e) => {
     res.status(400).send();
   })
