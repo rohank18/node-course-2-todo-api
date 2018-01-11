0a99c1cb8404df5b0c2c394693b9ca9f522becb3
rohan <rohankhunteta3000@gmail.com>
2018-01-12 01:06:48 +0530

    Setup

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://admin:admin@ds151207.mlab.com:51207/todoapp1234' || 'mongodb://localhost:27017/TodoApp');
+
+if (Process.env.PORT) {
+  mongoose.connect('mongodb://admin:admin@ds151207.mlab.com:51207/todoapp1234');
+} else {
+  mongoose.connect('mongodb://localhost:27017/TodoApp');
+}
 
 module.exports = {mongoose};
