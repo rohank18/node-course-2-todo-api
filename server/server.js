d71c482b137f82800cc7f51530e287b7f153df84
rohan <rohankhunteta3000@gmail.com>
2018-01-12 18:44:00 +0530

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
- var express = require('express');
+var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
 
@@ -47,6 +47,24 @@ app.get('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
   })
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send(todo);
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
